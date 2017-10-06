256c130ed6a3f3f8e4e67cfe05fdd55bb4ca46c5
Ivan Caminal <ivancaminal72@gmail.com>
2017-10-06 22:48:59 +0200

    frequency of appearance calculation

--- DatasetAnalysis.m
+++ DatasetAnalysis.m
@@ -1,16 +1,19 @@
 
-function dataset_analisis = DatasetAnalysis(directory)
-  % call with > da = DatasetAnalysis('datasets/train')
+function [dataset_analisis, signal_freq] = DatasetAnalysis(directory)
+  % call with -> [da, freq] = DatasetAnalysis('datasets/train')
 
   addpath('evaluation');
 
   files = ListFiles(directory);
   % dataset_description = zeros(,10)
-
+    
+  signal_freq = containers.Map;
+  number_signals = 0;
+  
   for i=1:size(files,1)
     mask = imread(strcat(directory, '/mask/mask.', files(i).name(1:size(files(i).name,2)-3), 'png'));
     [annotations signs] = LoadAnnotations(strcat(directory, '/gt/gt.', files(i).name(1:size(files(i).name,2)-3), 'txt')); 
-
+    
     i
     
     % There can be many signs in one picture, n_signs is the number of signs in this picture
@@ -30,10 +33,24 @@ function dataset_analisis = DatasetAnalysis(directory)
         
       % Add structure
       dataset_analisis(i) = struct('name', files(i).name, 'x', ann.x, 'y', ann.y, 'w', ann.w, 'h', ann.h, 'form_factor', form_factor, 'filling_ratio', filling_ratio);
-      
+    end
+    
+    % Count how many signals of specific signal type are
+    for  k=1:size(signs, 1)
+        number_signals=number_signals+1;
+        if isKey(signal_freq, signs{k})
+            signal_freq(signs{k}) = signal_freq(signs{k})+1;
+        else
+            signal_freq(signs{k}) = 1;
+        end
     end
   end
-
+  sf_keys = keys(signal_freq);
+  
+  % Iterate map to normalize frequency of appearance
+  for m = 1:length(signal_freq)
+     signal_freq(sf_keys{m}) = signal_freq(sf_keys{m})/number_signals;
+  end
 end
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -44,6 +61,6 @@ end
 
 
 function fr = FillingRatio(signal, w, h)
-    %The nnz function counts the number of non zero values of the matrix
+    % The nnz function counts the number of non zero values of the matrix
     fr = nnz(signal)/(w*h);
 end
