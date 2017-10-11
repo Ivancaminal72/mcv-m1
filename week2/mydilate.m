04b190f0bc85b34f53ad106ad427033ee5411d05
Ivan Caminal <ivancaminal72@gmail.com>
2017-10-11 19:56:57 +0200

    improve mydilate function

--- week2/mydilate.m
+++ week2/mydilate.m
@@ -1,10 +1,25 @@
 function output = mydilate(input, se)
+    %Binarize the structural element
+    
     maxSize = max(size(se));
-    if rem(maxSize,2)==0
-        error('The se provided can not be a structural element');
+    minSize = min(size(se));
+    %Normalize the structural element
+    if maxSize ~= minSize
+        if maxSize == size(se,1)
+            se = [se, zeros(maxSize, maxSize-minSize)];
+        else
+            se = [se; zeros(maxSize-minSize, maxSize)];
+        end
     end
-    %Normalize and Rotate the structural element
-    n_se = zeros(maxSize);
+    %Redefine the structural element to have a center
+    if rem(maxSize,2)==0 
+        n_se = zeros(maxSize+1);
+        se = [se,zeros(maxSize,1); zeros(1,maxSize+1)];
+        maxSize = maxSize+1;
+    else
+        n_se = zeros(maxSize);
+    end
+    %Rotate the structural element 180º
     nr_se = n_se;
     n_se = n_se | se;
     for i=1:size(n_se,1)
@@ -12,16 +27,17 @@ function output = mydilate(input, se)
             nr_se(i,j) = n_se(maxSize-i+1, maxSize-j+1);
         end
     end
-    padding = (((maxSize+1)/2)-1);
-    %Add padding to the imput matrix
+    nr_se = uint8(nr_se);
+    padding = ((maxSize+1)/2)-1;
+    output = uint8(zeros(size(input)));
+    %Add padding to the imput
     input = [zeros(padding,size(input,2)+padding*2); 
         [zeros(size(input,1),padding), input, zeros(size(input,1),padding)]; 
         zeros(padding,size(input,2)+padding*2)];
     %Compute the output result
-    output = zeros(size(input));
     for i=1:size(output,1)
         for j=1:size(output,2)
-            output(i,j)=max(max(input(i:maxSize, j:maxSize) & nr_se));
+            output(i,j)=max(max(input(i:i+maxSize-1, j:j+maxSize-1) .* nr_se));
         end
     end    
 end
