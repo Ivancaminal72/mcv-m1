e37c30fb24cf51c312f3276e8eac398a258080cc
Marc Assens <marc.a.r95@gmail.com>
2017-10-11 23:15:59 +0200

    Color filtering with HSV + YCbCr space

--- week1/CandidateGenerationPixel_Color.m
+++ week1/CandidateGenerationPixel_Color.m
@@ -1,13 +1,14 @@
 
 function [pixelCandidates] = CandidateGenerationPixel_Color(im, space)
-
-    im=double(im);
-
     switch space
         case 'normrgb'
-            [pixelCandidates] = ThresholdsStrategy2(im);
+            im=double(im);
+            [pixelCandidates] = ThresholdsStrategy(im);
         case 'gaussian_thresholds'    
+            im=double(im);
             [pixelCandidates] = GaussianFitWithThresholds(im);
+        case 'hsv_ycbcr'
+            [pixelCandidates] = HSVStrategy(im);
         otherwise
             error('Incorrect color space defined');
             return
@@ -21,7 +22,7 @@ function [pixelCandidates] = ThresholdsStrategy(im)
 
     %% Method 1 (Tresholds by colors) %%
     %Find pixel candidates according to different colors
-    pixelCandidates = pixelCandidates | (im(:,:,1)>210 & im(:,:,2)<15 & im(:,:,3)<15); %Redish colors
+    pixelCandidates = pixelCandidates | (im(:,:,1)>210 & im(:,:,2)<70 & im(:,:,3)<60); %Redish colors
     pixelCandidates = pixelCandidates | (im(:,:,1)>15 & im(:,:,2)<90 & im(:,:,3)<190); %Blueish colors
     pixelCandidates = pixelCandidates | (im(:,:,1)>15 & im(:,:,2)<15 & im(:,:,3)<15); %Blackish colors
 end
@@ -32,8 +33,9 @@ function [pixelCandidates] = ThresholdsStrategy2(im)
 
     %% Method 1 (Tresholds by colors) %%
     %Find pixel candidates according to different colors
-    pixelCandidates = pixelCandidates | (im(:,:,1)>70 & im(:,:,2)<40 & im(:,:,3)<40); %Redish colors
+    pixelCandidates = pixelCandidates | (im(:,:,1)>70 & im(:,:,2)<70 & im(:,:,3)<60); %Redish colors
     pixelCandidates = pixelCandidates | (im(:,:,1)<40 & im(:,:,2)<100 & im(:,:,3)>70); %Blueish colors
+
 end
 
 function [pixelCandidates] = GaussianFitWithThresholds(im)
@@ -45,4 +47,19 @@ function [pixelCandidates] = GaussianFitWithThresholds(im)
     for i=1:size(thresholds,2)
         pixelCandidates = pixelCandidates | (im(:,:,1) > thresholds(i).r_min & im(:,:,1)<thresholds(i).r_max & im(:,:,2)>thresholds(i).g_min & im(:,:,2)<thresholds(i).g_max & im(:,:,3)>thresholds(i).b_min & im(:,:,3)<thresholds(i).b_max ); 
     end
+end
+
+function [pixelCandidates] = HSVStrategy(im)
+    im_hsv = rgb2hsv(im);
+    mask_hsv_red = (im_hsv(:,:,1)<0.03 | im_hsv(:,:,1)>0.9); %Redish colors
+    mask_hsv_blue = (im_hsv(:,:,1)<0.75 & im_hsv(:,:,1)>0.55); %Blueish colors
+
+    im_ycbcr = rgb2ycbcr(im);
+    mask_ycbcr_red = (im_ycbcr(:,:,3)<175 & im_ycbcr(:,:,3)>135); %Redish colors
+    mask_ycbcr_blue = (im_ycbcr(:,:,2)<175 & im_ycbcr(:,:,2)>135); %Blueish colors
+
+    mask_red = mask_hsv_red .* mask_ycbcr_red;
+    mask_blue = mask_hsv_blue .* mask_ycbcr_blue;
+
+    pixelCandidates = mask_red | mask_blue;
 end
