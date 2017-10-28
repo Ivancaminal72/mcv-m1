45a959d83012df7f479c1ed5dc4eadad6a050a93
Ivan Caminal <ivancaminal72@gmail.com>
2017-10-28 22:24:05 +0200

    slidingWindow init

--- week3/SlidingWindow.m
+++ week3/SlidingWindow.m
@@ -1,3 +1,30 @@
-function [wc] = SlidingWindow(mask, da, im)
+function [wc] = SlidingWindow(mask, da, jump, combinations)
     
+    [rows, cols] = size(mask);
+    vcomb = getCombinations(combinations);
+    
+    for i = 1:jump:rows-window_h+1
+        for j = 1:jump:cols-window_w+1
+            
+            sum = %TODO: obtain the sumatory
+            
+            
+        end
+    end
+end
+
+function vcomb = getCombinations(combinations)
+    if(mod(combinations,2)==0 && combinations > 1)
+        interval = (da('all').w_max-da('all').w_min)/combinations;
+        for d = 1:combinations
+            vcomb = [vcomb struct('w', (da('all').w_min)+interval*combinations, 'h', (da('all').h_min)+interval*combinations)];
+        end
+    elseif(combinations >1)
+        interval = (da('all').w_max-da('all').w_min)/(combinations-1);
+        for d = 1:combinations
+            vcomb = [vcomb struct('w', (da('all').w_min)+interval*combinations, 'h', (da('all').h_min)+interval*combinations)];
+        end
+    else
+        error('incorrect number of combinations');
+    end   
 end
